d2943efb595af0576792352765411ed4fae52a0d
DingleZhang94 <dingle.zhang@gmail.com>
2018-08-01 00:45:22 -0400

    Register and style

---
+++ src/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Register from './Register';
+
+
+class Main extends React.Component{
+    render(){
+        return (
+            <div className = "main">
+                <Register/>
+            </div>
+        );
+    }
+}
+
+export default Main;
